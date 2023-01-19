66041c40bb4aff9a07da33a8f1790e32ddd42693
Lee-Git22 <lee.alan9801@gmail.com>
2023-01-19 12:08:14 -0500

    Implement tic tac toe prototype

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -8,50 +8,90 @@
   # Check win con
 # Repeat until moves are at 9 or if win con is met
 module Game
-  def draw(multi_array)
-    puts '---------------'
-    for i in 0...multi_array.length do
-      puts "| #{multi_array[i][0]} || #{multi_array[i][1]} || #{multi_array[i][2]} |"
-    end
-    puts '---------------'
+  def draw(game_state)
+    puts <<-TEXT
+    +---------------+
+    || #{game_state['board'][0]} || #{game_state['board'][1]} || #{game_state['board'][2]} ||
+    || #{game_state['board'][3]} || #{game_state['board'][4]} || #{game_state['board'][5]} ||
+    || #{game_state['board'][6]} || #{game_state['board'][7]} || #{game_state['board'][8]} ||
+    +---------------+
+    TEXT
   end
 
   def player_input(game_state)
-    puts "Player #{game_state['player']}: Select position to take"
+    puts "Player #{game_state['player'] ? 1 : 2}: Select position to take"
     gets.to_i
   end
 
-  def update(game_state, input)
-    game_state['board'].map! do |row|
-      row.map! do |position|
-        if position == input
-          'X'
-        else
-          position
-        end
-      end
+  def update_board(game_state, input)
+    game_state['board'].map do |position|
+    if position == input 
+      game_state['turn'] += 1
+      game_state['player'] = !game_state['player']
+      game_state['player'] ? 'O' : 'X'
+    else
+      position
+    end
     end
   end
 
+  def check_winner(game_state)
+    WINNING_COMBO.each do |combo|
+      tmp_array = []
+      3.times do |position|
+        tmp_array.push([game_state['board'][combo[position] - 1]])
+      end
+      if tmp_array.count(tmp_array[0]) == 3
+        return game_state['winner'] = game_state['player'] ? 1 : 2
+      end
+    end
+    game_state['winner'] = nil
+  end
+  
 
   def play_turn(game_state)
-    draw(game_state['board'])
+    puts "turn is: #{game_state['turn']}"
+    draw(game_state)
+
     position = player_input(game_state)
-    game_state['board'] = update(game_state, position)
-    game_state['turn'] += 1
+    game_state['board'] = update_board(game_state, position)
+    draw(game_state)
+  end
+
+  def display_winner(game_state)
+    puts game_state['winner'] == 2 ? 'Player 1 Wins!' : 'Player 2 Wins!'
   end
 end
+
 
 include Game
 
 
 
 game_state = {
-  'board' => [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+  'board' => [1, 2, 3, 4, 5, 6, 7, 8, 9],
   'turn' => 1,
-  'winner' => '',
-  'player' => 1
+  'winner' => nil,
+  'player' => true
 }
 
-play_turn(game_state)
-draw(game_state['board'])
+WINNING_COMBO = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7]
+]
+
+### FOR DEBUGGING
+
+while game_state['turn'] <= 9 && game_state['winner'] == nil
+  play_turn(game_state)
+  game_state['winner'] = check_winner(game_state)
+end
+
+display_winner(game_state)
+
